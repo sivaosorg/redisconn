5915d91630ca22b6153ac51f76cfb7455ab163e9
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-06-20 21:24:28 +0700

    :sparkles: feat: added new client connection #1

--- go.mod
+++ go.mod
@@ -1,3 +1,26 @@
 module github.com/sivaosorg/redisconn
 
 go 1.20
+
+require github.com/sivaosorg/govm v1.0.3
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.8 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
--- redisconn/redisconn.go
+++ redisconn/redisconn.go
@@ -1,1 +1,46 @@
 package redisconn
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+	"github.com/sivaosorg/govm/dbx"
+	"github.com/sivaosorg/govm/logger"
+	"github.com/sivaosorg/govm/redisx"
+)
+
+var (
+	instance *redis.Client
+	_logger  = logger.NewLogger()
+)
+
+func NewClient(config redisx.RedisConfig) (*redis.Client, dbx.Dbx) {
+	s := dbx.NewDbx().SetDatabase(config.Database)
+	if !config.IsEnabled {
+		s.SetConnected(false).
+			SetMessage("Redis unavailable").
+			SetError(fmt.Errorf(s.Message))
+		return &redis.Client{}, *s
+	}
+	if instance != nil {
+		s.SetConnected(true)
+		return instance, *s
+	}
+	client := redis.NewClient(&redis.Options{
+		Addr:     config.UrlConn,
+		Password: config.Password,
+		DB:       0,
+	})
+	err := client.Ping().Err()
+	if err != nil {
+		s.SetConnected(false).SetError(err).SetMessage(err.Error())
+		return &redis.Client{}, *s
+	}
+	if config.DebugMode {
+		_logger.Info(fmt.Sprintf("Redis client connection:: %s", config.Json()))
+		_logger.Info(fmt.Sprintf("Connected successfully to redis cache:: %s/%s", config.UrlConn, config.Database))
+	}
+	instance = client
+	s.SetConnected(true).SetMessage("Connection established")
+	return instance, *s
+}
